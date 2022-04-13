fc9bc9474dcdc53b12b713eafed4838ea0c6380a
Otterly Useless <michael@hendrixweb.net>
2022-04-13 16:18:09 -0400

    beep boop

--- snippets/php/joomla_page-based-subtemplates.php
+++ snippets/php/joomla_page-based-subtemplates.php
@@ -3,8 +3,9 @@ $component       = $app->input->get('option', '', 'string');
 $viewtype        = $app->input->get('view', '', 'string');
 $special_layouts = array('com_content');
 $file_name       = 'index_default';
+?>
 
-if(in_array($component, $special_layouts))
+<?php if(in_array($component, $special_layouts))
 {
 	$comp_name     = substr($component, 4);
 	$tmp_file_name = 'index_' . Joomla\Filesystem\File::makeSafe($comp_name . '-' . $viewtype);
